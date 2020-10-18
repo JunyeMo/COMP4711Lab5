9e768560c1290a499eb448f91b708366b35ead0b
Jack_M <Jack_M@LAPTOP-A7R9C1EU>
2020-10-18 21:50:14 +0800

    add some color page

---
+++ app/Views/news/overview.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<h2><?= esc($title); ?></h2>
+
+<?php if (! empty($news) && is_array($news)) : ?>
+
+    <?php foreach ($news as $news_item): ?>
+
+        <h3><?= esc($news_item['title']); ?></h3>
+
+        <div class="main">
+            <?= esc($news_item['body']); ?>
+        </div>
+        <p><a href="/news/<?= esc($news_item['slug'], 'url'); ?>">View article</a></p>
+
+    <?php endforeach; ?>
+
+<?php else : ?>
+
+    <h3>No News</h3>
+
+    <p>Unable to find any news for you.</p>
+
+<?php endif ?>
---
+++ app/Views/news/view.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<h2><?= esc($news['title']); ?></h2>
+<?= esc($news['body']); ?>
